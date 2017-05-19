ec2ff255dbf94fc075ef4311f5d3549912cbebb7
RarexWU16 <info@danielboman.se>
2017-05-19 10:21:04 +0200

    Articles

---
+++ SqlWorkScheduler/SqlWorkScheduler.HttpListner/Actors/ArticleManagerActor.cs
@@ -0,0 +1,48 @@
+﻿using Akka.Actor;
+using SqlWorkScheduler.HttpReceiver.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlWorkScheduler.HttpReceiver.Actors
+{
+    class ArticleManagerActor : ReceiveActor
+    {
+        public class StaticActor
+        {
+            public static IActorRef ArticleActor { get; set; }
+        }
+        public ArticleManagerActor()
+        {
+            Receive<ArticleDataCmd>(cmd => ReceiveArticleDataCmd(cmd));
+        }
+        private void ReceiveArticleDataCmd(ArticleDataCmd cmd)
+        {
+            //Creating actorsystem
+            var system = ActorSystem.Create("ArticleSystem");
+
+            StaticActor.ArticleActor = system.ActorOf<ArticleActor>("ArticleActor");
+
+            //cmd contains all articles
+            //dict is a list of row values and columnnames as keys
+            var dict = new Dictionary<string, object>();
+
+            for (int i = 0; i < cmd.Row.ItemArray.Length; i++)
+            {
+                if (!(cmd.Row[i] is DBNull))
+                {
+                    dict.Add(cmd.Row.Table.Columns[i].ColumnName, cmd.Row[i]);
+                }
+                else
+                {
+                    dict.Add(cmd.Row.Table.Columns[i].ColumnName, null);
+                }
+            }
+            
+            StaticActor.ArticleActor.Tell(new ArticleCmd(dict));
+        }
+
+    }
+}
---
+++ SqlWorkScheduler/SqlWorkScheduler.HttpListner/Actors/DataReaderHandlerActor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+using SqlWorkScheduler.HttpReceiver.Messages;
+using Microsoft.SqlServer.Server;
+using System.Data;
+using Akka.Configuration;
+
+namespace SqlWorkScheduler.HttpReceiver.Actors
+{
+    public class StaticActor
+    {
+        public static IActorRef ArticleManagerActor { get; set; }
+    }
+
+    class DataReaderHandlerActor : ReceiveActor
+    {
+        public DataReaderHandlerActor()
+        {
+            Receive<ProcessDataCmd>(cmd => ReceiveProcessDataCmd(cmd));
+        }
+
+        private void ReceiveProcessDataCmd(ProcessDataCmd cmd)
+        {
+            //Creating actorsystem
+            var system = ActorSystem.Create("ArticleManagerSystem");
+
+            StaticActor.ArticleManagerActor = system.ActorOf<ArticleManagerActor>("ArticleManagerActor");
+
+            //All rows in the stream
+            var rows = cmd.Data.Rows;
+
+            for (int i = 0; i < rows.Count; i++)
+            {
+                StaticActor.ArticleManagerActor.Tell(new ArticleDataCmd(rows[i]));
+            }
+        }
+    }
+}
